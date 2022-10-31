ab47bba1bf461f886b03ee7a415867ef2a99326d
esiopaiva <esio_paiva@hotmail.com>
2022-10-31 01:39:45 +0000

    Adicionando o comando para -m para criar a pasta do usuário no sistema

--- provide_users.sh
+++ provide_users.sh
@@ -19,31 +19,31 @@ groupadd GRP_SEC
 
 echo "Criando Usuários...."
 
-useradd carlos -c "Carlos" -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
+useradd carlos -c "Carlos" -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
 passwd carlos -e
 
-useradd maria -c "Maria" -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
+useradd maria -c "Maria" -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
 passwd maria -e
 
-useradd joao -c "João" -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
+useradd joao -c "João" -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
 passwd joao -e
 
-useradd debora -c "Debora" -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
+useradd debora -c "Debora" -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
 passwd debora -e
 
-useradd sebastiana -c "Sebastiana" -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
+useradd sebastiana -c "Sebastiana" -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
 passwd sebastiana -e
 
-useradd roberto -c "Roberto" -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
+useradd roberto -c "Roberto" -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
 passwd roberto -e
 
-useradd josefina -c "Josefina" -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
+useradd josefina -c "Josefina" -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
 passwd josefina -e
 
-useradd amanda -c "Amanda" -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
+useradd amanda -c "Amanda" -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
 passwd amanda -e
 
-useradd rogerio -c "Rogério" -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
+useradd rogerio -c "Rogério" -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
 passwd rogerio -e
 
 echo "Definindo permissão dos diretórios..."
